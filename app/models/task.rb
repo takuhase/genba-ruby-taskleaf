d972f62e50353e5af7aadd642c04d275f0ab5073
takuhase <hasetakunoid3228@icloud.com>
2020-07-20 02:43:38 +0900

    Bugfix

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,11 +2,11 @@ class Task < ApplicationRecord
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
   # 検証用メソッドを定義
-  validate: validate_name_not_including_comma
+  validate :validate_name_not_including_comma
 
   private
 
   def validate_name_not_including_comma
-    errors.add(:name, 'にカンマを含めることはできません。') if name&.includes?(',')
+    errors.add(:name, 'にカンマを含めることはできません。') if name&.include?(',')
   end
 end
