25a3361c55934a67e438d832d6b3247e65f967a2
takuhase <hasetakunoid3228@icloud.com>
2020-07-21 03:32:26 +0900

    Refactor test code

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,25 +2,35 @@ require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
+    # letでユーザA, ユーザBを定義
+    let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
+    let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+
     before do
-      # ユーザA作成
-      user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
-      # ユーザAのタスク作成
+      # ユーザAが呼ばれ、タスクとともにDBに保存される
       FactoryBot.create(:task, name: '最初のタスク', user: user_a)
+
+      visit login_path
+      fill_in 'メールアドレス', with: login_user.email
+      fill_in 'パスワード', with: login_user.password
+      click_button 'ログインする'
     end
 
-    context 'ユーザーがログインしているとき' do
-      before do
-        # ユーザAでログイン
-        visit login_path
-        fill_in 'メールアドレス', with: 'a@example.com'
-        fill_in 'パスワード', with: 'password'
-        click_button 'ログインする'
-      end
+    context 'ユーザーAがログインしているとき' do
+      let(:login_user) { user_a }
 
       it 'ユーザAのタスクが表示される' do
         # タスク名が画面表示されていることを確認
         expect(page).to have_content '最初のタスク'
+      end
+    end
+
+    context 'ユーザーBがログインしているとき' do
+      let(:login_user) { user_b }
+
+      it 'ユーザーAが作成したタスクが表示されない' do
+        # ユーザーAのタスクが画面表示されないことを確認
+        expect(page).not_to have_content '最初のタスク'
       end
     end
   end
