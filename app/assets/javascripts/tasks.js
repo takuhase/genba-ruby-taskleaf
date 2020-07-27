ebfc0c27eb4abc6653413333b672275472e2cf30
takuhase <hasetakunoid3228@icloud.com>
2020-07-27 12:45:08 +0900

    Delete task with Ajax

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,4 +1,6 @@
 document.addEventListener('turbolinks:load', function() {
+  console.log('Loaded!!!!!!!!!!!!!!!');
+
   document.querySelectorAll('td').forEach(function(td) {
     td.addEventListener('mouseover', function(e) {
       e.currentTarget.style.backgroundColor = '#ea618e';
@@ -9,11 +11,11 @@ document.addEventListener('turbolinks:load', function() {
     });
   });
 
-  document.querySelectorAll('.delete').forEach(function(a) {
-    a.addEventListener('ajax:success', function() {
-      var td = a.parentNode;
-      var tr = td.parentNode;
-      tr.style.display = 'none';
-    });
-  });
+  // document.querySelectorAll('.delete').forEach(function(a) {
+  //   a.addEventListener('ajax:success', function() {
+  //     var td = a.parentNode;
+  //     var tr = td.parentNode;
+  //     tr.style.display = 'none';
+  //   });
+  // });
 });
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -48,7 +48,7 @@ class TasksController < ApplicationController
     @task.destroy
     # redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
     # Ajax仕様
-    head :no_content
+    # head :no_content
   end
 
   def confirm_new
